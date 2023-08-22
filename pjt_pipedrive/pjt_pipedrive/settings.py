5a3bbbf9de59a167110dbbced2701e6904440a34
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-22 17:24:35 -0300

    ajuste no diretorios static, parâmetrizado pelo arquivo confi_app.py e location statics no servidor.

--- pjt_pipedrive/pjt_pipedrive/settings.py
+++ pjt_pipedrive/pjt_pipedrive/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = config_app.STATIC_ROOT
+STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / "static"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # "/var/www/static/",
+]
 # STATICFILES_DIRS = [
 #     # BASE_DIR / "static",
 #     os.path.join(BASE_DIR, "app_webhook/static/css"),
