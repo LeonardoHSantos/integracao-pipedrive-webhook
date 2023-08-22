aa5898120529505b5904c21a406122a3f9d7448f
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-22 17:34:57 -0300

    ajuste no diretorios static, parâmetrizado pelo arquivo confi_app.py e location statics no servidor.

--- pjt_pipedrive/pjt_pipedrive/settings.py
+++ pjt_pipedrive/pjt_pipedrive/settings.py
@@ -122,18 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # "/var/www/static/",
+    os.path.join(BASE_DIR, "statics/css"),
+    os.path.join(BASE_DIR, "statics/img"),
+    # os.path.join(BASE_DIR, "base_statics/js"),
 ]
-# STATICFILES_DIRS = [
-#     # BASE_DIR / "static",
-#     os.path.join(BASE_DIR, "app_webhook/static/css"),
-#     os.path.join(BASE_DIR, "app_webhook/static/img"),
-#     # os.path.join(BASE_DIR, "base_statics/js"),
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
