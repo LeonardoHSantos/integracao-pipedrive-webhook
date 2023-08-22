52844882f47bd6e6b7502db5591d92560d3ae067
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-22 04:01:44 -0300

    removido diretório static/js de settings.py.

--- pjt_pipedrive/pjt_pipedrive/settings.py
+++ pjt_pipedrive/pjt_pipedrive/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "base_statics/css"),
     os.path.join(BASE_DIR, "base_statics/img"),
-    os.path.join(BASE_DIR, "base_statics/js"),
+    # os.path.join(BASE_DIR, "base_statics/js"),
 ]
 
 # Default primary key field type
