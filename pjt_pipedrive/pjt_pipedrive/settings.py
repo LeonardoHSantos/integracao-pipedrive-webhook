f510707550a7d0caf1fc948de9d41623aed89698
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-22 05:00:09 -0300

    diretório de arquivos estatícos movido para app.

--- pjt_pipedrive/pjt_pipedrive/settings.py
+++ pjt_pipedrive/pjt_pipedrive/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "base_statics_app_1/css"),
-#     os.path.join(BASE_DIR, "base_statics_app_1/img"),
-#     # os.path.join(BASE_DIR, "base_statics/js"),
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles/css"),
+    os.path.join(BASE_DIR, "staticfiles/img"),
+    # os.path.join(BASE_DIR, "base_statics/js"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
