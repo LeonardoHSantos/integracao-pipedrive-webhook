ba0125744a74b25aab6b654697a994f035ac5c6b
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-28 11:47:57 -0300

    melhorias na navbar e renomeado rotas de navegação do site. Ajuste no tamanho dos cards em home.

--- pjt_pipedrive/app_webhook/urls.py
+++ pjt_pipedrive/app_webhook/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about/", views.about, name="about"),
-    path("info-services/<str:service_name>", views.infoServices, name="info_services"),
+    path("sobre/", views.about, name="about"),
+    path("info-servicos/<str:service_name>", views.infoServices, name="info_services"),
     path("pipedrive/v1/person/", views.PipedrivePerson, name="person"),
     path("pipedrive/v1/info/person/<str:id_person>", views.infoPerson, name="info_person"),
     path("pipedrive/v1/deals/", views.PipedriveDeals, name="deals"),
