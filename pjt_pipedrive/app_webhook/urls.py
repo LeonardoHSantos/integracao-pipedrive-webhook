77d004f1847d339d08e033cbd462547d2bbe6672
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-08-30 19:01:09 -0300

    melhorias barra de navegação e nas páginas serviços e sobre.

--- pjt_pipedrive/app_webhook/urls.py
+++ pjt_pipedrive/app_webhook/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.home, name="login"),
+    path("register/", views.home, name="register"),
     path("sobre/", views.about, name="about"),
     path("info-servicos/<str:service_name>", views.infoServices, name="info_services"),
     path("pipedrive/v1/person/", views.PipedrivePerson, name="person"),
