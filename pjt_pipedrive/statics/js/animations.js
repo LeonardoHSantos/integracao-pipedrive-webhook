c8a8785d3618f21ac14bd9e9e6d3b4adcd90b6ee
Leonardo Santos <leonardodossantoss50@gmail.com>
2023-09-03 20:39:22 -0300

    adicionado animações no scroll das páginas. Animações left, right e botton.

---
+++ pjt_pipedrive/statics/js/animations.js
@@ -0,0 +1,20 @@
+// [data-animation-scroll]
+// [data-animation-scroll="cards-services-left"]
+// [data-animation-scroll="cards-services-right"]
+// [data-animation-scroll="cards-services-botton"]
+// [data-animation-scroll].animate
+const target = document.querySelectorAll('[data-animation-scroll]');
+function animationScroll(){
+const windownEixo_Y = window.pageYOffset + 540;
+    target.forEach(function(e){
+        if ( (windownEixo_Y) > e.offsetTop){
+        e.classList.add("animate");
+        } else {
+        e.classList.remove("animate");
+        }
+    });
+}
+animationScroll();
+window.addEventListener("scroll", function(){
+    animationScroll();
+});
